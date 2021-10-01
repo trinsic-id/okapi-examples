4f1ab61c723acbee5c60652e704d998627df3f88
Tomislav Markovski <tmarkovski@gmail.com>
2021-10-01 11:30:40 -0400

    Add NET example for Greeter service using DIDComm (#4)

---
+++ dotnet/Greeter/Client/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using Google.Protobuf;
+using Okapi.Transport;
+
+namespace Client
+{
+    public static class Extensions
+    {
+        public static T Extract<T>(this CoreMessage didcommMessage) where T : IMessage<T>, new()
+        {
+            var result = new T();
+            result.MergeFrom(didcommMessage.Body);
+            return result;
+        }
+    }
+}
---
+++ dotnet/Greeter/Common/TestKeys.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Multiformats.Base;
+using Okapi.Keys;
+
+namespace Common
+{
+    public static class Alice
+    {
+        const string KeyId = "did:key:z6LSskw3r3441B8YnhcB1LKxmRqpEiyZH3Prpb6No1fHTze6#z6LSskw3r3441B8YnhcB1LKxmRqpEiyZH3Prpb6No1fHTze6";
+        public static JsonWebKey SecretKey = new JsonWebKey
+        {
+            Kid = KeyId,
+            Kty = "OKP",
+            Crv = "X25519",
+            D = "oLio1cRmNIgQePHHJFeCdw0-j4ZtJBH0HVQU8fXahVs"
+        };
+        public static JsonWebKey PublicKey = new JsonWebKey
+        {
+            Kid = KeyId,
+            Kty = "OKP",
+            Crv = "X25519",
+            X = "7vLvUiaXEUgajINpIomBTwQtQCnf-r7cptpKTEWHzjs"
+        };
+    }
+
+    public static class Bob
+    {
+        const string KeyId = "did:key:z6LSgKhoYDRJJaJ84wHjnW67r8RTaLExHhzrFZgd3Hh1md7j#z6LSgKhoYDRJJaJ84wHjnW67r8RTaLExHhzrFZgd3Hh1md7j";
+
+        public static JsonWebKey SecretKey = new JsonWebKey
+        {
+            Kid = KeyId,
+            Kty = "OKP",
+            Crv = "X25519",
+            D = "cLH0xA7mdSp5zcxSOVepnMuFtJSWZtRI0PoR2cET420"
+        };
+        public static JsonWebKey PublicKey = new JsonWebKey
+        {
+            Kid = KeyId,
+            Kty = "OKP",
+            Crv = "X25519",
+            X = "RQvhX5wPpmHdd97FxvC_xK3GB0VpyLhN5xQN45EwjXY"
+        };
+    }
+}
