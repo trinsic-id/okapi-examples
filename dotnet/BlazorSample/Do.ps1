3cd0395550efa12b12993eda826d0d7aa9e3c929
Tomislav Markovski <tomislav@trinsic.id>
2021-12-02 19:00:55 -0500

    Finish code

--- dotnet/BlazorSample/Do.ps1
+++
@@ -1,7 +0,0 @@
-rm -fr ../nuget/*
-cp /Users/geddoff/Git/trinsic-id/okapi/dotnet/Library/Okapi/bin/Release/Okapi.Net.2.0.0.* ../nuget/
-dotnet nuget locals all --clear
-rm -fr bin
-rm -fr obj
-
-dotnet restore
